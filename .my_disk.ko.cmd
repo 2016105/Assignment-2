30209e3250155a35e2f8a918ad1c0fabf0f7e04c
h2016105 <h2016105@pilani.bits-pilani.ac.in>
2017-04-09 14:06:42 +0530

    Delete .my_disk.ko.cmd

--- .my_disk.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/tanya84/disk_on_ram/my_disk.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/tanya84/disk_on_ram/my_disk.ko /home/tanya84/disk_on_ram/my_disk.o /home/tanya84/disk_on_ram/my_disk.mod.o
